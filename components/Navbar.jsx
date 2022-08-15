6b92668be921bdb282faf5058dafdf2948eb3c35
rima1881 <110826233+rima1881@users.noreply.github.com>
2022-08-15 05:42:16 -0400

    new file:   components/Navbar.jsx 	modified:   next.config.js 	modified:   package-lock.json 	modified:   package.json 	new file:   pages/_document.js 	deleted:    pages/api/hello.js 	new file:   pages/api/search.js 	deleted:    public/favicon.ico 	new file:   public/images/Blue_logo.png 	new file:   public/images/main.jpg 	deleted:    public/vercel.svg 	deleted:    styles/Home.module.css 	modified:   styles/globals.css 	new file:   styles/navbar.module.css

---
+++ components/Navbar.jsx
@@ -0,0 +1,27 @@
+import style from '../styles/navbar.module.css'
+import Image from 'next/image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+
+export default function Navbar(){
+    return(
+        <nav className={style.navbar}>
+            <picture>
+                <img src='/images/blue_logo.png' className={style.logo} alt='asda'/>
+            </picture>
+            <ul className={style.list}>
+                <li>Home</li>
+                <li>Events</li>
+                <li>Our Mission</li>
+                <li>Projects</li>
+                <li>3D Printing Services</li>
+                <li>Sponsors</li>
+                <li>Contact us</li>
+            </ul>
+
+            <span className={style.search}>
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </span>
+        </nav>
+    )
+}
